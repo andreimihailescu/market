16de111670575731699070d446195b03db82998b
REINET\andrei.mihailescu <andrei.mihailescu@rei-d-services.com>
2019-04-17 16:25:22 +0300

    Npm run dev

--- public/js/app.js
+++ public/js/app.js
@@ -49635,8 +49635,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! E:\Virtual machines\Vagrant\Homestead\laravel\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! E:\Virtual machines\Vagrant\Homestead\laravel\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\Andrei\laravelVagrant\laravel\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\Andrei\laravelVagrant\laravel\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
