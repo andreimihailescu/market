60956b19833553ff48f937d84be98f3e59f218b3
REINET\andrei.mihailescu <andrei.mihailescu@rei-d-services.com>
2019-04-17 13:06:01 +0300

    Create products table

--- database/migrations/2019_04_17_081957_create_products_table.php
+++ database/migrations/2019_04_17_081957_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->enum('role', ['normal', 'admin'])->default('normal');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->text('description')->nullable();
+            $table->enum('type', ['laptop', 'computer', 'phone']);
+            $table->integer('stock')->unsigned()->default(0);
+            $table->integer('price')->unsigned();
+            $table->string('image_source');
             $table->timestamps();
         });
     }
@@ -32,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 }
