92fea795bd8a430299fda0ed101ae87f8020d872
REINET\andrei.mihailescu <andrei.mihailescu@rei-d-services.com>
2019-04-17 16:20:32 +0300

    Default user seed password update

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,7 +17,7 @@ class UsersTableSeeder extends Seeder
 		$user->name = 'Andrei';
 		$user->email = 'andrei@email.com';
 		$user->role = 'normal';
-		$user->password = '123123';
+		$user->password = '$2y$10$4y/4EKVAXkkO7GMYqtJvq.H0IQKqPFToyngK1MT77UmsJsIGyCsz6'; // test1234
 		$user->save();
 		$user->cart()->save(new Cart());
 	}
