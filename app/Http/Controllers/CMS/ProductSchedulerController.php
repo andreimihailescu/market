25753c2b93ca0088d1e1c00a4a31476ab957845a
andrei.mihailescu <andrei.mihailescu@rei-d-services.com>
2019-07-02 13:33:28 +0300

    ProductSchduler controller and model changes

--- app/Http/Controllers/CMS/ProductSchedulerController.php
+++ app/Http/Controllers/CMS/ProductSchedulerController.php
@@ -2,84 +2,85 @@
 
 namespace App\Http\Controllers\CMS;
 
+use App\ProductSchedulerTask;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class ProductSchedulerController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
+		//
+	}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create()
+	{
+		//
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        dd($request);
-    }
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param \Illuminate\Http\Request $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request)
+	{
+		dd($request);
+	}
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param \App\ProductSchedulerTask $productSchedulerTask
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show(ProductSchedulerTask $productSchedulerTask)
+	{
+		//
+	}
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param \App\ProductSchedulerTask $productSchedulerTask
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit(ProductSchedulerTask $productSchedulerTask)
+	{
+		//
+	}
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param \Illuminate\Http\Request $request
+	 * @param \App\ProductSchedulerTask $productSchedulerTask
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, ProductSchedulerTask $productSchedulerTask)
+	{
+		//
+	}
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param \App\ProductSchedulerTask $productSchedulerTask
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy(ProductSchedulerTask $productSchedulerTask)
+	{
+		//
+	}
 }
