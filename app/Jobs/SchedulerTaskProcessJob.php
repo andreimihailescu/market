2dff7c06272e88ea17a7981b7ed97b9a6a464912
andrei.mihailescu <andrei.mihailescu@rei-d-services.com>
2019-07-12 16:02:31 +0300

    Product Scheduler - CRON

--- app/Jobs/SchedulerTaskProcessJob.php
+++ app/Jobs/SchedulerTaskProcessJob.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Product;
 use App\SchedulerTask;
 use DateTime;
 use Illuminate\Bus\Queueable;
@@ -9,35 +10,52 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Date;
 
 class SchedulerTaskProcessJob implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+	use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    /**
-     * Create a new job instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
+	/**
+	 * Create a new job instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		//
+	}
 
-    /**
-     * Execute the job.
-     *
-     * @return void
-     */
-    public function handle()
-    {
+	/**
+	 * Execute the job.
+	 *
+	 * @return void
+	 */
+	public function handle()
+	{
 //        $tasks = SchedulerTask::with('action', 'action.product')->get();
-        $tasks = SchedulerTask::all();
+		$tasks = SchedulerTask::all();
 
-        foreach($tasks as $task){
-        	dd(new DateTime($task->date));
+		foreach ($tasks as $task) {
+			$taskDate = new DateTime($task->date);
+
+			if ($taskDate < new DateTime() || !isset($task->action)) {
+				continue;
+			}
+
+			switch ($task->action->type) {
+				case 'price_change':
+					$this->priceChange($task->action->product_id, $task->action->new_price);
+					break;
+				default:
+			}
 		}
+	}
 
-        dd(json_decode(json_encode($tasks), true));
-    }
+	private function priceChange(int $targetProductId, int $newPrice): void
+	{
+		$targetProduct = Product::get($targetProductId);
+
+		echo "Changing price of the product $targetProduct->name to price $newPrice";
+	}
 }
